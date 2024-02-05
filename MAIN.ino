1d49d409a479c0c17d41616d01676dfe94f5430f
mihkuno <caindayjoeninyo@gmail.com>
2024-02-05 22:38:32 +0800

    added class groups

--- MAIN.ino
+++ MAIN.ino
@@ -1,35 +1,113 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-
-#define temperatureSensor = A0; // try.. works on digital pins only
-#define turbiditySensor = A1;
-#define aciditySensor = A3;
-
-
-// ====================================
-//               TEMPERATURE
-// ====================================
-
-
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire temperatureWire(temperatureSensor);
-
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature temperature(&temperatureWire);
-
-
+// Define pin numbers for sensors
+#define TEMPERATURE_SENSOR A0
+#define TURBIDITY_SENSOR A1
+#define ACIDITY_SENSOR A3
 
 // ====================================
-//               ACIDITY
+//             TemperatureSensor
 // ====================================
 
-float acidityActualValue;
-float acidtyCalibrationValue = 23.1;
-unsigned long int acidityAverageValue;
-int buffer_arr[10], acidityTemperatureValue;
+class TemperatureSensor {
+private:
+  OneWire oneWire;
+  DallasTemperature temperatureSensor;
 
+public:
+  // Constructor initializes the OneWire and DallasTemperature objects
+  TemperatureSensor(int pin) : oneWire(pin), temperatureSensor(&oneWire) {
+    temperatureSensor.begin();
+  }
 
+  // Method to get temperature in Celsius
+  float getTemperatureCelsius() {
+    temperatureSensor.requestTemperatures();
+    return temperatureSensor.getTempCByIndex(0);
+  }
+
+  // Method to get temperature in Fahrenheit
+  float getTemperatureFahrenheit() {
+    temperatureSensor.requestTemperatures();
+    return temperatureSensor.getTempFByIndex(0);
+  }
+};
+
+// Create an instance of TemperatureSensor
+TemperatureSensor tempSensor(TEMPERATURE_SENSOR);
+
+// ====================================
+//              AciditySensor
+// ====================================
+
+class AciditySensor {
+private:
+  float actualValue;
+  float calibrationValue = 23.1;
+  unsigned long int averageValue;
+  int buffer[10], temperatureValue;
+
+public:
+  // Constructor
+  AciditySensor() {}
+
+  // Method to perform acidity measurement
+  void measure() {
+    for (int i = 0; i < 10; i++) {
+      buffer[i] = analogRead(ACIDITY_SENSOR);
+      delay(30);
+    }
+
+    // Sort buffer array in ascending order
+    for (int i = 0; i < 9; i++) {
+      for (int j = i + 1; j < 10; j++) {
+        if (buffer[i] > buffer[j]) {
+          temperatureValue = buffer[i];
+          buffer[i] = buffer[j];
+          buffer[j] = temperatureValue;
+        }
+      }
+    }
+
+    averageValue = 0;
+    for (int i = 2; i < 8; i++)
+      averageValue += buffer[i];
+
+    float voltage = (float)averageValue * 5.0 / 1024 / 6;
+    actualValue = -5.70 * voltage + calibrationValue;
+
+    Serial.println("pH Val: ");
+    Serial.print(actualValue);
+  }
+};
+
+// Create an instance of AciditySensor
+AciditySensor aciditySensor;
+
+// ====================================
+//             TurbiditySensor
+// ====================================
+
+class TurbiditySensor {
+private:
+  int sensorPin;
+
+public:
+  // Constructor
+  TurbiditySensor(int pin) : sensorPin(pin) {
+    pinMode(sensorPin, INPUT);
+  }
+
+  // Method to perform turbidity measurement
+  int measure() {
+    // Read the input on the analog pin
+    return analogRead(sensorPin);
+  }
+};
+
+// Create an instance of TurbiditySensor
+TurbiditySensor turbiditySensor(TURBIDITY_SENSOR);
 
 // ====================================
 //               SETUP
@@ -37,76 +115,26 @@ int buffer_arr[10], acidityTemperatureValue;
 
 void setup() {
   Serial.begin(115200);
-
-   temperature.begin();
-   pinMode(turbiditySensor, INPUT);
-   pinMode(aciditySensor, INPUT);
-
 }
-
 
 // ====================================
 //               LOOP
 // ====================================
 
-
 void loop() {
-
-   // ====================================
-   //               TEMPERATURE
-   // ====================================
-
-   // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  temperature.requestTemperatures(); 
-  
+  // Temperature measurement
   Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(tempSensor.getTemperatureCelsius());
   Serial.print(" - Fahrenheit temperature: ");
-  Serial.println(sensors.getTempFByIndex(0));
+  Serial.println(tempSensor.getTemperatureFahrenheit());
 
+  // Acidity measurement
+  aciditySensor.measure();
 
-   // ====================================
-   //               ACIDITY
-   // ====================================
+  // Turbidity measurement
+  int turbidityValue = turbiditySensor.measure();
+  Serial.println("Turbidity Value: ");
+  Serial.println(turbidityValue);
 
-   for (int i = 0; i < 10; i++) {
-      buffer_arr[i] = analogRead(aciditySensor);
-      delay(30);
-   }
-
-   for (int i = 0; i < 9; i++) {
-      for (int j = i + 1; j < 10; j++) {
-         if (buffer_arr[i] > buffer_arr[j]) {
-         acidityTemperatureValue = buffer_arr[i];
-         buffer_arr[i] = buffer_arr[j];
-         buffer_arr[j] = acidityTemperatureValue;
-         }
-      }
-   }
-
-   acidityAverageValue = 0;
-   for (int i = 2; i < 8; i++)
-      acidityAverageValue += buffer_arr[i];
-
-   float voltage = (float)acidityAverageValue * 5.0 / 1024 / 6;
-   acidityActualValue = -5.70 * voltage + acidtyCalibrationValue;
-
-   Serial.println("pH Val: ");
-   Serial.print(acidityActualValue);
-   
-
-
-
-   // ====================================
-   //               TURBIDITY
-   // ====================================
-
-   // read the input on analog pin 0:
-   int turbidityValue = analogRead(turbiditySensor);
-   // print out the value you read:
-   Serial.println(turbidityValue);
-
-   delay(100);
+  delay(100);
 }
-
