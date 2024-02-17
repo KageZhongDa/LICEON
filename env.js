d94825125d2d04916cb7db2433351b70546b7b5f
KageZhongDa <160244222+KageZhongDa@users.noreply.github.com>
2024-02-18 07:02:07 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // DO NOT TOUCH THIS UNLESS REQUIRED
 export const IP_ESP32_LICEON = '192, 168, 192, 168'; // ip addr of the esp32
-export const SSID_ESP32_LICEON = 'KASERVPOT'; // ssid connected to the esp32 the esp32 (pass: password)
+export const SSID_ESP32_LICEON = 'KASERVPOT'; // ssid connected to the esp32 (pass: password)
 
 // THIS MIGHT CHANGE ON KAGE LAPTOP SERVER WHEN HOSTING
 export const IP_KAGE_SERVER = '192.168.192.168'; // change into the ip of the server(windows ipconfig, mac ipconfig getifaddr,linux ifconfig)
